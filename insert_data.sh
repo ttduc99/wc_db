c2893bd793935620f6544616f7527e8a53a9b65b
ttduc99 <ttduc999@gmail.com>
2025-10-02 23:38:31 -0400

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,30 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+$PSQL "TRUNCATE TABLE games, teams;"
+
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WGOALS OGOALS
+do
+  if [[ $YEAR != 'year' ]]
+  then
+    # Winner team
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $WINNER_ID ]]
+    then
+      $PSQL "INSERT INTO teams(name) VALUES('$WINNER')"
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # Opponent team
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')"
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # Insert game row
+    $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+           VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WGOALS, $OGOALS)"
+  fi
+done
